6984982bbb4bb0185e9377ce28d92813a6f5301b
Langloie <Langloiea@gmail.com>
2022-05-26 15:03:12 -0400

    server now has a route

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 //online marketplace (like facebook marketplace)
 
+//add required packages
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+const PORT = 3000;
+
+//parse request of type json
+app.use(express.json());
+
+
+
 // get all listings
 //  url/listings/
 
+app.get('/listings', (req, res)=>{
+    res.json({'message': "listings page"})
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
 
 // create new listing
 //  url/listings/new
