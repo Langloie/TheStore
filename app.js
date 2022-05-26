afbc2f8430f953dff0c89b5861cd53baed1b389f
Langloie <Langloiea@gmail.com>
2022-05-26 11:17:09 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+//online marketplace (like facebook marketplace)
+
+// get all listings
+//  url/listings/
+
+
+// create new listing
+//  url/listings/new
+
+//delete listing
+//  url/listings/:id
+
+//update listing (price reduction or new description)
+//  url/listings/:id
+
+//add create listing form on main page
+//  url/listings
+
+//edit entire listing
+//  url/listings/:id/edit
+
+//show one listing in the browser
+//  url/listings/:id
